36b448424959cc87a053f036f02bc0ae192ad8bf
Siddhesh More <113719150+Msiddhesh008@users.noreply.github.com>
2024-03-17 23:15:08 +0530

    second commit

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import logo from "../Images/logoDark.png";
+import logoMini from "../Images/logoDarkMini.png";
 import { useDispatch } from "react-redux";
 import { loginUser } from "../Redux/Slice/auth";
 import Button02 from "../Components/Buttons/Button02";
@@ -10,7 +11,7 @@ import { nav } from "../Routes/Nav";
 
 const Dashboard = () => {
   const distach = useDispatch();
-  const [isDrawerOpen, setIsDrawerOpen] = useState(true);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   const [openDrawerClick, setOpenDrawerClick] = useState(false);
 
   const openDrawerOnClick = () => {
@@ -35,9 +36,9 @@ const Dashboard = () => {
       <aside
         className="h-100 pointer position-relative"
         onMouseOver={() => setIsDrawerOpen(true)}
-        onMouseLeave={() => setIsDrawerOpen(true)}
+        onMouseLeave={() => setIsDrawerOpen(false)}
         style={{
-          width: isDrawerOpen || openDrawerClick ? 140 : 80,
+          width: isDrawerOpen || openDrawerClick ? 160 : 66,
           transition: "width 0.3s ease-in-out", // Smooth transition for width change
           overflow: "hidden", // Hide overflow to prevent content overflow during transition
         }}
@@ -46,19 +47,34 @@ const Dashboard = () => {
           <img
             onClick={openDrawerOnClick}
             style={{
-              width: 100,
+              width: isDrawerOpen || openDrawerClick ? 80 : 35,
             }}
-            src={logo}
+            src={isDrawerOpen || openDrawerClick ? logo : logoMini}
             alt="Logo"
           />
         </div>
         <div className="p-2">
-          {nav.map(({ title, path }) => (
+          {nav.map(({ title, path, icon }) => (
             <div className=" mb-2 w-100 d-flex ">
-              <NavLink 
-
-              className="link p-2  rounded-4   w-100 ps-3 " to={path}>
-                {title}
+              <NavLink
+              style={{
+                height:40
+              }}
+                className="link p-2  rounded-4 fs-6 d-flex align-items-center gap-2 w-100 ps-3 "
+                to={path}
+              >
+                <span
+                  style={{
+                    display: isDrawerOpen || openDrawerClick ? "flex" : "flex",
+                  }}
+                >{icon}</span>
+                <span
+                  style={{
+                    display: isDrawerOpen || openDrawerClick ? "block" : "none",
+                  }}
+                >
+                  {title}
+                </span>
               </NavLink>
             </div>
           ))}
@@ -88,8 +104,8 @@ const Dashboard = () => {
       <main
         className=" h-100 rounded-5"
         style={{
-          backgroundColor:"#FAFBFC",
-          width: `calc(100% - ${isDrawerOpen || openDrawerClick ? 140 : 80}px)`,
+          backgroundColor: "#FAFBFC",
+          width: `calc(100% - ${isDrawerOpen || openDrawerClick ? 160 : 66}px)`,
           transition: "width 0.3s ease-in-out",
           overflow: "auto",
         }}
--- src/Routes/Nav.js
+++ src/Routes/Nav.js
@@ -1,19 +1,26 @@
+import { LuLayoutDashboard } from "react-icons/lu";
+import { MdSmartButton } from "react-icons/md";
+import { FaTable } from "react-icons/fa6";
+
 export const nav = [
-    {
-      title: "Dashboard",
-      path: "/",
-      // icon: <GridViewOutlinedIcon />,
-    },
-    {
-      title: "Buttons",
-      path: "/buttons",
-    },
-    {
-      title: "Tables",
-      path: "/tables",
-    },
-    {
-      title: "Kanban",
-      path: "/kanban",
-    },
-]
+  {
+    title: "Dashboard",
+    path: "/",
+    icon: <LuLayoutDashboard style={{ background: "-webkit-linear-gradient(#5E7FE7, #D86572)", WebkitBackgroundClip: "text", WebkitTextFillColor: "transparent" }} />,
+  },
+  {
+    title: "Buttons",
+    path: "/buttons",
+    icon: <MdSmartButton style={{ background: "-webkit-linear-gradient(#5E7FE7, #D86572)", WebkitBackgroundClip: "text", WebkitTextFillColor: "transparent" }} />,
+  },
+  {
+    title: "Tables",
+    path: "/tables",
+    icon: <FaTable style={{ background: "-webkit-linear-gradient(#5E7FE7, #D86572)", WebkitBackgroundClip: "text", WebkitTextFillColor: "transparent" }} />,
+  },
+  {
+    title: "Kanban",
+    path: "/kanban",
+    icon: <LuLayoutDashboard style={{ background: "-webkit-linear-gradient(#5E7FE7, #D86572)", WebkitBackgroundClip: "text", WebkitTextFillColor: "transparent" }} />,
+  },
+];
